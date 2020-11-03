b2d5a067621c1213ea83b8816d87e7c822635743
Auston Stewart <auston@shedbuilt.net>
2020-11-02 20:17:12 -1000

    Building differently named binaries for different platforms

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ done
 # Configure
 for SHED_PKG_LOCAL_OPTION in "${!SHED_PKG_LOCAL_OPTIONS[@]}"; do
     case "$SHED_PKG_LOCAL_OPTION" in
-        sun50ih5)
+        sun50ia64)
             SHED_PKG_ATF_PLATFORM='sun50i_a64'
             SHED_PKG_ATF_TARGET='bl31'
             SHED_PKG_ATF_PRODUCT='build/sun50i_a64/debug/bl31.bin'
@@ -22,4 +22,4 @@ done
 # Build and Install
 make PLAT="$SHED_PKG_ATF_PLATFORM" DEBUG=1 "$SHED_PKG_ATF_TARGET" &&
 install -dm755 "${SHED_FAKE_ROOT}/boot/u-boot" &&
-install -m644 "$SHED_PKG_ATF_PRODUCT" "${SHED_FAKE_ROOT}/boot/u-boot"
+install -m644 "$SHED_PKG_ATF_PRODUCT" "${SHED_FAKE_ROOT}/boot/u-boot/${SHED_PKG_ATF_PLATFORM}-${SHED_PKG_ATF_TARGET}.bin"
