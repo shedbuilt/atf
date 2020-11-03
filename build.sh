25a4780d77922c147ca27f0823bf3a1cf8318019
Auston Stewart <auston@shedbuilt.net>
2020-11-02 20:27:45 -1000

    Fixing rk3328 bl31 product path

--- build.sh
+++ build.sh
@@ -15,11 +15,12 @@ for SHED_PKG_LOCAL_OPTION in "${!SHED_PKG_LOCAL_OPTIONS[@]}"; do
         rk3328)
             SHED_PKG_ATF_PLATFORM='rk3328'
             SHED_PKG_ATF_TARGET='bl31'
-            SHED_PKG_ATF_PRODUCT='build/rk3328/debug/bl31.bin'
+            SHED_PKG_ATF_PRODUCT='build/rk3328/debug/bl31/bl31.elf'
             ;;
     esac
 done
 # Build and Install
+SHED_PKG_LOCAL_ATF_NAME=$(basename "$SHED_PKG_ATF_PRODUCT")
 make PLAT="$SHED_PKG_ATF_PLATFORM" DEBUG=1 "$SHED_PKG_ATF_TARGET" &&
 install -dm755 "${SHED_FAKE_ROOT}/boot/u-boot" &&
-install -m644 "$SHED_PKG_ATF_PRODUCT" "${SHED_FAKE_ROOT}/boot/u-boot/${SHED_PKG_ATF_PLATFORM}-${SHED_PKG_ATF_TARGET}.bin"
+install -m644 "$SHED_PKG_ATF_PRODUCT" "${SHED_FAKE_ROOT}/boot/u-boot/${SHED_PKG_ATF_PLATFORM}-${SHED_PKG_LOCAL_ATF_NAME}"
